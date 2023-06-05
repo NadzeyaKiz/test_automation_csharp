14de538c0b945cd7862be1e952156e13353c1231
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-06-05 17:59:01 +0100

    First draft (Task_01-06 are done)

---
+++ Code/CSharpBasics_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using CSharpBasics_2;
+
+Console.WriteLine("Tasks Execution!");
+var homework_2 = new Homework_2();
+
+
+//Console.WriteLine("Execution of task_01:");
+//homework_2.Task_01();
+//Console.WriteLine("Execution of task_01 is finished");
+
+//Console.WriteLine("Execution of task_02:");
+//homework_2.Task_02();
+//Console.WriteLine("Execution of task_02 is finished");
+
+//Console.WriteLine("Execution of task_03:");
+//homework_2.Task_03();
+//Console.WriteLine("Execution of task_03 is finished");
+
+//Console.WriteLine("Execution of task_04:");
+//homework_2.Task_04();
+//Console.WriteLine("Execution of task_04 is finished");
+
+//Console.WriteLine("Execution of task_05:");
+//homework_2.Task_05();
+//Console.WriteLine("Execution of task_05 is finished");
+
+//Console.WriteLine("Execution of task_06:");
+//homework_2.Task_06();
+//Console.WriteLine("Execution of task_06 is finished");
+
+
+Console.WriteLine("Execution of task_07:");
+homework_2.Task_07();
+Console.WriteLine("Execution of task_07 is finished");
+
+
+
