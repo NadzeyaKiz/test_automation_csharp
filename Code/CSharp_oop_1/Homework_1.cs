0684fb402abedfee15c20ede6fddaf2b9e00447d
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-06-12 17:48:49 +0100

    CSharpOOP Homework Task_1

---
+++ Code/CSharp_oop_1/Homework_1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_oop_1
+{
+    internal class Homework_1
+    {
+        public double CalculeteTotalAmount(double itemQuantity, double itemPrice, double enteredDiscount)
+        {
+            double totalWithoutDiscount = itemQuantity * itemPrice;
+            double discountAmount = totalWithoutDiscount*enteredDiscount/100;
+            double TotalAmount = totalWithoutDiscount - discountAmount;
+            return TotalAmount;           
+        }
+    }
+}
---
+++ Code/CSharp_oop_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using CSharp_oop_1;
+
+Console.WriteLine("Tasks Execution!");
+var homework_1 = new Homework_1();
+
+# region Task 01
+Console.WriteLine("Execution of task_01:");
+
+Console.WriteLine("Please enter the product quantity");
+double itemQuantity = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please enter the price for one item");
+double itemPrice = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please enter the discount");
+double enteredDiscount = Convert.ToDouble(Console.ReadLine());
+var TotalAmount = homework_1.CalculeteTotalAmount(itemQuantity, itemPrice, enteredDiscount);
+Console.WriteLine("---------------------");
+Console.WriteLine($@"The total amount for {itemQuantity} items 
+with price of {itemPrice} for item, 
+and discount {enteredDiscount} - is {TotalAmount};");
+Console.WriteLine("---------------------");
+Console.WriteLine("Execution of task_01 is finished");
+
+#endregion
