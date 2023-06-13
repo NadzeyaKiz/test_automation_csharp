42a168518d29eeaad5b59a999d2faf1fa1618735
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-06-13 23:59:16 +0100

    Task_2 added

--- Code/CSharp_oop_1/Homework_1.cs
+++ Code/CSharp_oop_1/Homework_1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,34 @@ namespace CSharp_oop_1
 {
     internal class Homework_1
     {
-        public double CalculeteTotalAmount(double itemQuantity, double itemPrice, double enteredDiscount)
+        public double CalculateTotalAmount(double itemQuantity, double itemPrice, double enteredDiscount)
         {
             double totalWithoutDiscount = itemQuantity * itemPrice;
-            double discountAmount = totalWithoutDiscount*enteredDiscount/100;
+            double discountAmount = Math.Round((totalWithoutDiscount*enteredDiscount/100), 2);
             double TotalAmount = totalWithoutDiscount - discountAmount;
             return TotalAmount;           
+        }
+        public double CalculeteTotalAmountForEachItemAndReceiptTotal(double[] toPayForItems)                    
+        {
+            double receiptTotal = 0;
+            for (int i = 0; i < toPayForItems.Length; i++)
+                receiptTotal += toPayForItems[i];   
+            
+            return receiptTotal;
+        }
+
+        public void PrintReceipt(string[] itemNames, double[] itemPricies, double[] itemQuantities, double[] toPayForItems)
+        {          
+            Console.WriteLine($"---------------------------------------------------");
+            Console.WriteLine("{0, -20}{1, 10}{2, 15}{3, 15}", "Name", "Price", "ItemCount", "SubTotalItem");
+
+            for ( int i = 0; i < itemNames.Length; i++ )
+                Console.WriteLine($"{itemNames[i],-20}{itemPricies[i],10}{itemQuantities[i],15}{toPayForItems[i],15}");
+
+            var receiptTotal = CalculeteTotalAmountForEachItemAndReceiptTotal(toPayForItems);
+
+            Console.WriteLine($@"The amount to pay is {receiptTotal}");
+            Console.WriteLine($"---------------------------------------------------");
         }
     }
 }
--- Code/CSharp_oop_1/Program.cs
+++ Code/CSharp_oop_1/Program.cs
@@ -4,20 +4,53 @@ Console.WriteLine("Tasks Execution!");
 var homework_1 = new Homework_1();
 
 # region Task 01
-Console.WriteLine("Execution of task_01:");
+//Console.WriteLine("Execution of task_01:");
+//Console.WriteLine("Please enter the product quantity");
+//double itemQuantity = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Please enter the price for one item");
+//double itemPrice = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Please enter the discount");
+//double enteredDiscount = Convert.ToDouble(Console.ReadLine());
+//var TotalAmount = homework_1.CalculateTotalAmount(itemQuantity, itemPrice, enteredDiscount);
+//Console.WriteLine("---------------------");
+//Console.WriteLine($@"The total amount for {itemQuantity} items 
+//with price of {itemPrice} for item, 
+//and discount {enteredDiscount} - is {TotalAmount};");
+//Console.WriteLine("---------------------");
+//Console.WriteLine("Execution of task_01 is finished");
+#endregion
 
-Console.WriteLine("Please enter the product quantity");
-double itemQuantity = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Please enter the price for one item");
-double itemPrice = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Please enter the discount");
+#region Task 02
+Console.WriteLine("Execution of task_02:");
+
+Console.WriteLine("Please enter how many items do you have in your basket:");
+int numberOfItems = Convert.ToInt32(Console.ReadLine());
+
+string[] itemNames = new string[numberOfItems];
+double[] itemPricies = new double[numberOfItems];
+double[] itemQuantities = new double[numberOfItems];
+for (int i = 0; i < numberOfItems; i++)
+{
+    Console.WriteLine($"Please enter {i} item name");
+    itemNames[i] = Console.ReadLine();
+    Console.WriteLine($"Please enter {i} item price");
+    itemPricies[i] = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Please enter {i} item number of items");
+    itemQuantities[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+Console.WriteLine("Please enter the discount:");
 double enteredDiscount = Convert.ToDouble(Console.ReadLine());
-var TotalAmount = homework_1.CalculeteTotalAmount(itemQuantity, itemPrice, enteredDiscount);
-Console.WriteLine("---------------------");
-Console.WriteLine($@"The total amount for {itemQuantity} items 
-with price of {itemPrice} for item, 
-and discount {enteredDiscount} - is {TotalAmount};");
-Console.WriteLine("---------------------");
-Console.WriteLine("Execution of task_01 is finished");
+
+double[] toPayForItems = new double[numberOfItems];
+for (int i = 0; i < numberOfItems; i++)
+{
+    toPayForItems[i] = homework_1.CalculateTotalAmount(itemQuantities[i], itemPricies[i], enteredDiscount);
+}
+
+
+homework_1.PrintReceipt(itemNames, itemPricies, itemQuantities, toPayForItems);
+
+
 
 #endregion
