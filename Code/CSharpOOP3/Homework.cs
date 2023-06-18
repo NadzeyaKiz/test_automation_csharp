0c8cf2ad32798c37e1d2f0a2bb3b8a198f4ec83f
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-06-18 11:39:03 +0100

    CSharp OOP3 Task_01 is completed

---
+++ Code/CSharpOOP3/Homework.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP3
+{
+    public class Flowers  //Task_01
+    {
+        public string color;
+        public double price;
+
+        public string Color//(setter-only property)
+        {
+            set { color = value; }
+        }
+
+        public double Price { get; }//(read-only property)
+
+
+    }    
+}
---
+++ Code/CSharpOOP3/Program.cs
@@ -0,0 +1,22 @@
+﻿using CSharpOOP3;
+
+Flowers flowers = new Flowers();
+
+// Попытка записи значения в поле color
+flowers.color = "Red"; // Error Line: Field 'Flowers.color' is inaccessible due to its protection level
+
+// Попытка чтения значения из поля color
+string flowerColor = flowers.color; // Error Line: Field 'Flowers.color' is inaccessible due to its protection level
+Console.WriteLine("Flower color: " + flowerColor);
+
+// Запись значения в свойство Color
+flowers.Color = "Blue";
+
+// Попытка записи значения в свойство Price
+flowers.Price = 10.99; // Error Line: Property or indexer 'Flowers.Price' cannot be assigned to -- it is read only
+
+// Чтение значения из свойства Price
+double flowerPrice = flowers.Price;
+
+
+Console.WriteLine("Flower price: " + flowerPrice);
