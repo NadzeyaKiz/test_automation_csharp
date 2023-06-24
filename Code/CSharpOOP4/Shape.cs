64f74efb49504fe1e3ee470a0eeb9462b430946e
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-06-24 22:56:51 +0100

    Task_01 is completed

---
+++ Code/CSharpOOP4/Shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP4
+{
+    abstract class Shape
+    {
+        private string _name;
+
+        public Shape(string name)
+        {
+            _name = name;
+        }
+
+        public abstract double CalculateVolume();
+
+        public virtual void DisplayShapeInfo()
+        {
+            Console.WriteLine("Shape: " + _name);
+            Console.WriteLine("Volume: " + CalculateVolume());
+        }
+    }
+
+}
