4d68a464f16b31da3b5e624d009d37c93fd0861e
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-06-25 17:18:31 +0100

    Adding the program file to the hometask

--- Code/CSharpOOP4/Program.cs
+++ Code/CSharpOOP4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharpOOP4;
+
+#region Calculation of the Ball shape
+Console.WriteLine("----------------------");
+Ball ball = new Ball(10);
+ball.DisplayShapeInfo();
+Console.WriteLine("----------------------");
+#endregion
+
+#region Calculation of the Cylinder shape
+Console.WriteLine("----------------------");
+Cylinder cylinder = new Cylinder("Cylinder", 10, 5);
+cylinder.DisplayShapeInfo();
+Console.WriteLine("----------------------");
+#endregion
+
+#region Calculation of the Piramid shape
+Console.WriteLine("----------------------");
+Piramid piramid = new Piramid("Piramid", 46, 4);
+piramid.DisplayShapeInfo();
+Console.WriteLine("----------------------");
+#endregion
