3948b0b63d0e4b1b2546b4c3507374640f905a17
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-06-26 22:35:28 +0100

    Recommendations applied

---
+++ Code/CSharpOOP3/FlowerHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpOOP3
+{
+    public class FlowerHelper
+    {
+        //The GenerateRandomColor method generates a random color from a predefined list and returns it as a string
+        public static string GenerateRandomColor()
+        {
+            Random random = new Random();
+            string[] colors = { "Red", "Blue", "Yellow", "Pink", "Purple" };
+            int randomIndex = random.Next(colors.Length);
+            return colors[randomIndex];
+        }
+        public static void PrintFlowerInfo(string name, string color, double price)
+        {
+            Console.WriteLine($"Flower Name is {name}");
+            Console.WriteLine($"Flower Color is {color}");
+            Console.WriteLine($"Flower Price is {price}");
+            Console.WriteLine($"Flower Kind is {Flowers.Type}");
+        }
+        public static bool IsPriceExpensive(double price)
+        {           
+            return price > 50;
+        }
+        public static string CapitalizeFirstLetter(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return char.ToUpper(input[0]) + input.Substring(1);
+        }
+    }
+}
