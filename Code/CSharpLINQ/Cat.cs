8a89f0413cfd24d968a55dfba019b8b98b62c8a5
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-07-01 23:29:59 +0100

    CSharpLinq homework (Task01-09)

---
+++ Code/CSharpLINQ/Cat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLINQ
+{
+    internal class Cat
+    {
+        public string Color { get; set; }
+
+
+        public void Task_09(string[] args)
+        {
+            List<string> colors = new List<string> { "grey", "white", "black" };
+            List<Cat> cats = colors.Select(color => new Cat { Color = color }).ToList();
+
+            Console.WriteLine("The list of cats: ");
+
+            foreach (var cat in cats)
+            {
+                Console.WriteLine("Color is: " + cat.Color);
+            }
+        }
+        public void Task_010(string[] args)
+        {
+            List<Cat> cats = new List<Cat>
+            {
+                new Cat { Color = "gray" },
+                new Cat { Color = "white" },
+                new Cat { Color = "black" }
+            };
+
+        }
+    }    
+}
---
+++ Code/CSharpLINQ/Program.cs
@@ -0,0 +1,78 @@
+﻿using CSharpLINQ;
+using System;
+
+Console.WriteLine("Tasks Execution!");
+Homework homework = new Homework();
+
+Console.WriteLine("Execution of task_01:" );
+Console.WriteLine("---------------------");
+homework.Task_01();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_01");
+Console.WriteLine();
+
+Console.WriteLine("Execution of task_02:");
+Console.WriteLine("---------------------");
+homework.Task_02();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_02");
+Console.WriteLine();
+
+Console.WriteLine("Execution of task_03:");
+Console.WriteLine("---------------------");
+homework.Task_03();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_03");
+Console.WriteLine();
+
+Console.WriteLine("Execution of task_04:");
+Console.WriteLine("---------------------");
+homework.Task_04();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_04");
+Console.WriteLine();
+
+Console.WriteLine("Execution of task_05:");
+Console.WriteLine("Enter a string that contains words written in uppercase and lowercase letters.");
+Console.WriteLine("---------------------");
+homework.Task_05();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_05");
+Console.WriteLine();
+
+Console.WriteLine("Execution of task_06:");
+Console.WriteLine("---------------------");
+homework.Task_06();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_06");
+Console.WriteLine();
+
+Console.WriteLine("Execution of task_07:");
+Console.WriteLine("Please enter an integer number: ");
+Console.WriteLine("---------------------");
+homework.Task_07();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_07");
+Console.WriteLine();
+
+Console.WriteLine("Execution of task_08:");
+Console.WriteLine("---------------------");
+homework.Task_08();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_08");
+Console.WriteLine();
+
+Console.WriteLine("Execution of task_09:");
+Console.WriteLine("---------------------");
+homework.Task_09();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_09");
+Console.WriteLine();
+
+Console.WriteLine("Execution of task_10:");
+Console.WriteLine("---------------------");
+homework.Task_10();
+Console.WriteLine("---------------------");
+Console.WriteLine("End of execution of task_10");
+Console.WriteLine();
+
