aba462ff0a31ede9645edc506cbbbe615b0ce663
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-07-02 18:57:17 +0100

    CSharpException Homework is completed

---
+++ Code/CSharpException/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpException
+{
+    public class Triangle
+    {
+        public Triangle(double sideA, double sideB, double sideC) 
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                throw new ArgumentException("Side value cannot be equal to or less than zero");
+            }
+            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+            {
+                throw new ArgumentException("The sum of two sides must be greater than the third side. Please enter a valid values of the triangle sides.");
+            }
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+        public double SideA { get; private set; }
+        public double SideB { get; private set; }
+        public double SideC { get; private set; }
+
+        public void DisplaySides()
+        {
+            Console.WriteLine($"The sideA of the triangle is: {SideA}");
+            Console.WriteLine($"The sideB of the triangle is: {SideB}");
+            Console.WriteLine($"The sideC of the triangle is: {SideC}");
+        }
+    }
+}
