418c748162a9b3c0beea7a850d4faf303a35ee9f
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-07-04 10:47:17 +0100

    Coments are applied

--- Code/CSharpException/Triangle.cs
+++ Code/CSharpException/Triangle.cs
@@ -11,14 +11,15 @@ namespace CSharpException
     {
         public Triangle(double sideA, double sideB, double sideC) 
         {
-            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            if (!ValidateSidesNotNull(sideA, sideB, sideC))
             {
                 throw new ArgumentException("Side value cannot be equal to or less than zero");
             }
-            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+            if (!ValidateSidesSummOfSides(sideA, sideB, sideC))
             {
                 throw new ArgumentException("The sum of two sides must be greater than the third side. Please enter a valid values of the triangle sides.");
             }
+
             SideA = sideA;
             SideB = sideB;
             SideC = sideC;
@@ -32,6 +33,26 @@ namespace CSharpException
             Console.WriteLine($"The sideA of the triangle is: {SideA}");
             Console.WriteLine($"The sideB of the triangle is: {SideB}");
             Console.WriteLine($"The sideC of the triangle is: {SideC}");
+        }
+
+        public bool ValidateSidesNotNull (double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidateSidesSummOfSides(double sideA, double sideB, double sideC)
+        {
+            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
