ded4ac45f6fc2c4a7d329dde75b2dc4ed8f2df93
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-07-09 18:55:42 +0100

    CSharp_Patterns Task1 is completed

---
+++ Code/CSharp_Patterns/Decorator/ExtraLoudGigantosaurusDecorator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Patterns.Decorator
+{
+    /*Class decorator, also responsible for increasing the volume of the Gigantosaurus roar. This class is a concrete 
+     * decorator in the Decorator pattern structure. It wraps the base Gigantosaurus object and adds some extra 
+     * functionality, in this case even more roar volume boost.*/
+    public class ExtraLoudGigantosaurusDecorator : IGigantosaurus
+    {
+        private readonly IGigantosaurus _gigantosaurus;
+
+        public ExtraLoudGigantosaurusDecorator(IGigantosaurus gigantosaurus)
+        {
+            _gigantosaurus = gigantosaurus;
+        }
+
+        public void Roar()
+        {
+            Console.WriteLine("ExtraLoudGigantosaurusDecorator: ROOOOAAAARRRR!!!!");
+            _gigantosaurus.Roar();
+        }
+    }
+}
---
+++ Code/CSharp_Patterns/Program.cs
@@ -0,0 +1,38 @@
+﻿using CSharp_Patterns;
+using CSharp_Patterns.Decorator;
+
+#region Implementation of the Decorator pattern
+
+Console.WriteLine("Execution of the Task1. Implementation of the Decorator pattern");
+IGigantosaurus basicGigantosaurus = new Gigantosaurus();
+Console.WriteLine("------------------------");
+Console.WriteLine("The basic volume of the roar of the 'Gigantosaurus'");
+basicGigantosaurus.Roar();
+Console.WriteLine("------------------------");
+// Output: Gigantosaurus: Roar!
+
+IGigantosaurus loudGigantosaurus = new LoudGigantosarusDecorator(basicGigantosaurus);
+Console.WriteLine("------------------------");
+Console.WriteLine("Increased the volume of the roar of the 'Gigantosaurus' to 'Loud'");
+loudGigantosaurus.Roar();
+Console.WriteLine("------------------------");
+// Output:
+// LoudGigantosaurusDecorator: ROAR!!!
+// Gigantosaurus: Roar!
+
+IGigantosaurus extraLoudGigantosaurus = new ExtraLoudGigantosaurusDecorator(loudGigantosaurus);
+Console.WriteLine("------------------------");
+Console.WriteLine("Increased the volume of the roar of the 'Gigantosaurus' to 'Loud' to 'ExtraLoud'");
+extraLoudGigantosaurus.Roar();
+Console.WriteLine("------------------------");
+Console.WriteLine("End of execution of the Task1");
+// Output:
+// ExtraLoudGigantosaurusDecorator: ROOOOAAAARRRR!!!!
+// Gigantosaurus: Roar!
+
+/*Both decorator classes inherit from IGigantosaurus to maintain interface and compatibility with the Gigantosaurus 
+ * base class. They decorate the base Gigantosaurus object by adding a new behavior (increasing the volume of the roar) 
+ * without changing the Gigantosaurus itself. Thus, they make it easy to add new functionality to the base object, applying 
+ * the open/close principle and avoiding the need to change the base class itself.*/
+
+#endregion
