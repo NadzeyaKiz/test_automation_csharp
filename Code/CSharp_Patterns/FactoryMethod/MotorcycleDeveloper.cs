716c8a124dc7bce9ab95707c245ef74d35ec591c
Nadzeya Kizer <nadin1kiz@gmail.com>
2023-07-09 23:26:09 +0100

    CSharp_Patterns is completed

---
+++ Code/CSharp_Patterns/FactoryMethod/MotorcycleDeveloper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Patterns.FactoryMethod
+{/* The MotorcycleDeveloper class is the Creator of Motorcycle objects.
+    Implements the IDeveloper interface and provides a Create() method that returns an object of type IVehicle 
+    (Motorcycle).*/
+    internal class MotorcycleDeveloper : IDeveloper
+    {
+        public IVehicle Create()
+        {
+            return new Motorcycle();
+        }
+    }
+}
--- Code/CSharp_Patterns/Program.cs
+++ Code/CSharp_Patterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CSharp_Patterns;
 using CSharp_Patterns.Decorator;
+using CSharp_Patterns.FactoryMethod;
 
 #region Implementation of the Decorator pattern
 
@@ -26,6 +27,7 @@ Console.WriteLine("Increased the volume of the roar of the 'Gigantosaurus' to 'L
 extraLoudGigantosaurus.Roar();
 Console.WriteLine("------------------------");
 Console.WriteLine("End of execution of the Task1");
+
 // Output:
 // ExtraLoudGigantosaurusDecorator: ROOOOAAAARRRR!!!!
 // Gigantosaurus: Roar!
@@ -34,5 +36,42 @@ Console.WriteLine("End of execution of the Task1");
  * base class. They decorate the base Gigantosaurus object by adding a new behavior (increasing the volume of the roar) 
  * without changing the Gigantosaurus itself. Thus, they make it easy to add new functionality to the base object, applying 
  * the open/close principle and avoiding the need to change the base class itself.*/
+
+#endregion
+
+#region Factory Method
+
+// Creation of developers and use of their methods Create(), which returns an object of type IVehicle
+Console.WriteLine();
+Console.WriteLine("Execution of the Task2. Factory method");
+Console.WriteLine("------------------------");
+Console.WriteLine("The information about created unicycle object is: ");
+IDeveloper unicycleDeveloper = new UnicycleDeveloper();
+IVehicle unicycle = unicycleDeveloper.Create();
+unicycle.GetInfo();
+
+Console.WriteLine();
+
+Console.WriteLine("The information about created motorcycle object is: ");
+IDeveloper motorcycleDeveloper = new MotorcycleDeveloper();
+IVehicle motorcycle = motorcycleDeveloper.Create();
+motorcycle.GetInfo();
+
+Console.WriteLine();
+
+Console.WriteLine("The information about created car object is: ");
+IDeveloper carDeveloper = new CarDeveloper();
+IVehicle car = carDeveloper.Create();
+car.GetInfo();
+
+Console.WriteLine();
+
+Console.WriteLine("The information about created truck object is: ");
+IDeveloper truckDeveloper = new TruckDeveloper();
+IVehicle truck = truckDeveloper.Create();
+truck.GetInfo();
+
+Console.WriteLine("------------------------");
+Console.WriteLine("End of execution of the Task2");
 
 #endregion
